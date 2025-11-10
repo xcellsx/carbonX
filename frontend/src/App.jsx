e92b864234e6d3ad9c0cff91a5133947e68928b2
Celine Goh <celstudyies@gmail.com>
2025-11-10 15:36:28 +0800

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import SignupPage from './components/Auth/SignupPage';
 import LoginPage from './components/Auth/LoginPage';
 import CompanyInfoPage from './components/Company/CompanyInfoPage';
@@ -11,7 +11,6 @@ import NetworkPage from './components/Network/NetworkPage';
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -23,7 +22,6 @@ function App() {
         <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/network" element={<NetworkPage />} />
       </Routes>
-    </Router>
   );
 }
 
