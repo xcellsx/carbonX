cee12f547171ea050d10368135ba7a4c8e500a59
Misfit 1-3 <mhilmansabli@gmail.com>
2026-02-20 11:21:31 +0800

    updated graph fetch by product ID

--- backend/src/main/java/com/ecapybara/CarbonX/controller/GraphController.java
+++ backend/src/main/java/com/ecapybara/CarbonX/controller/GraphController.java
@@ -45,4 +45,58 @@ public class GraphController {
 
         return response.get("result");
     }
+
+    // @GetMapping ("/getentiregraph")
+    // public Object getGraphbyName(@RequestParam String database, @RequestParam String graphname) {
+    
+    
+    //     Map<String, Object> response = graphService.executeQuery(database, query);
+
+    //     return response.get("result");
+    // }
+
+    @GetMapping ("/productgraph")
+        public Object getProductGraph(@RequestParam String database, @RequestParam String productid) {
+        //currently hardcoded to use products,processes,inputs and outputs, can be improved later
+        String targetNode = "products/"+productid;
+        String edgeList = "inputs, outputs";
+
+        String query =
+        "LET targetNode = \"" + targetNode + "\" " +
+
+        "LET traversal = ( " +
+        "FOR v, e, p IN 1..100 INBOUND targetNode " +
+        edgeList + " " +
+        "FILTER e != null " +
+        "RETURN { " +
+            "node: { " +
+                "id: v._id, " +
+                "label: HAS(v, \"name\") ? v.name : v._key, " +
+                "type: PARSE_IDENTIFIER(v._id).collection " +
+            "}, " +
+            "edge: { " +
+                "source: e._from, " +
+                "target: e._to, " +
+                "type: PARSE_IDENTIFIER(e._id).collection " +
+            "} " +
+        "} " +
+        ") " +
+
+        "LET nodes = UNIQUE(traversal[* RETURN CURRENT.node]) " +
+        "LET links = UNIQUE(traversal[* RETURN CURRENT.edge]) " +
+
+        "LET rootNode = { " +
+            "id: targetNode, " +
+            "label: HAS(DOCUMENT(targetNode), \"name\") " +
+                "? DOCUMENT(targetNode).name " +
+                ": PARSE_IDENTIFIER(targetNode).key, " +
+            "type: PARSE_IDENTIFIER(targetNode).collection " +
+        "} " +
+
+        "RETURN { nodes: APPEND(nodes, rootNode, true), links: links }";
+
+        Map<String, Object> response = graphService.executeQuery(database, query);
+
+        return response.get("result");
+    }
 }
